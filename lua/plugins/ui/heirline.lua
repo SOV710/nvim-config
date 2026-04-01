1bf0848a6c10203631ea5d6c71107ba699036669
SOV710 <chris916911179@outlook.com>
2026-04-01 09:25:24 +0000

    style(plugins/ui): standardize string quotes from double quotes to single quotes in heirline.lua and noice.lua
    The change standardizes the string quoting style across the codebase to use single quotes for consistency. This improves code readability and maintainability by having a uniform style.

--- lua/plugins/ui/heirline.lua
+++ lua/plugins/ui/heirline.lua
@@ -1,25 +1,25 @@
 return {
-  "rebelot/heirline.nvim",
-  event = "VeryLazy",
-  dependencies = { "folke/snacks.nvim" },
+  'rebelot/heirline.nvim',
+  event = 'VeryLazy',
+  dependencies = { 'folke/snacks.nvim' },
   config = function()
-    local colors = require("plugins.ui.heirline.colors")
-    local statusline = require("plugins.ui.heirline.statusline")
-    local tabline_mod = require("plugins.ui.heirline.tabline")
+    local colors = require 'plugins.ui.heirline.colors'
+    local statusline = require 'plugins.ui.heirline.statusline'
+    local tabline_mod = require 'plugins.ui.heirline.tabline'
 
     -- Always show tabline (even with 1 tab, we show the window list)
     vim.o.showtabline = 2
 
-    require("heirline").setup({
+    require('heirline').setup {
       statusline = statusline,
       tabline = tabline_mod.tabline,
       opts = { colors = colors.get() },
-    })
+    }
 
-    vim.api.nvim_create_autocmd("ColorScheme", {
-      group = vim.api.nvim_create_augroup("heirline-colors", { clear = true }),
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      group = vim.api.nvim_create_augroup('heirline-colors', { clear = true }),
       callback = function()
-        require("heirline.utils").on_colorscheme(colors.get)
+        require('heirline.utils').on_colorscheme(colors.get)
       end,
     })
   end,
