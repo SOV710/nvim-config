4dfc7277fd18d31df23bd8eb01596d8c406db805
SOV710 <chris916911179@outlook.com>
2026-04-01 08:51:23 +0000

    feat(editor): add snacks.nvim plugin for word reference navigation
    This adds a new plugin configuration for word navigation functionality. The plugin provides keybindings to jump between word references with `]]` for next and `[[` for previous, supporting both normal and terminal modes. The configuration enables word highlighting with a debounce delay and notifications when reaching the end of references.

---
+++ lua/plugins/editor/words.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    words = {
+      enabled = true,
+      debounce = 200,          -- ms to wait before highlighting
+      notify_jump = false,     -- don't show notification when jumping
+      notify_end = true,       -- notify when reaching last/first reference
+    },
+  },
+  keys = {
+    { "]]", function() Snacks.words.jump(vim.v.count1) end, desc = "Next reference", mode = { "n", "t" } },
+    { "[[", function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev reference", mode = { "n", "t" } },
+  },
+}
