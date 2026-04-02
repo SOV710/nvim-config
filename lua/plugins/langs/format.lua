861a01321ceaef37bf5aa3bb416d6fe0f50debf6
SOV710 <chris916911179@outlook.com>
2026-04-02 08:31:28 +0000

    fix(mason,format): use opts function to defer core.language evaluation to load time

--- lua/plugins/langs/format.lua
+++ lua/plugins/langs/format.lua
@@ -2,13 +2,15 @@ return {
   "stevearc/conform.nvim",
   event = { "BufWritePre" },
   cmd = { "ConformInfo" },
-  opts = {
-    formatters_by_ft = require("core.language").formatters,
-    format_on_save = {
-      timeout_ms = 3000,
-      lsp_format = "fallback",
-    },
-  },
+  opts = function()
+    return {
+      formatters_by_ft = require("core.language").formatters,
+      format_on_save = {
+        timeout_ms = 3000,
+        lsp_format = "fallback",
+      },
+    }
+  end,
   keys = {
     {
       "<leader>cf",
--- lua/plugins/langs/mason.lua
+++ lua/plugins/langs/mason.lua
@@ -10,11 +10,13 @@ return {
     dependencies = { "williamboman/mason.nvim" },
     cmd = { "MasonToolsInstall", "MasonToolsUpdate" },
     event = "VeryLazy",
-    opts = {
-      ensure_installed = require("core.language").mason,
-      auto_update = false,
-      run_on_start = true,
-      start_delay = 3000,
-    },
+    opts = function()
+      return {
+        ensure_installed = require("core.language").mason,
+        auto_update = false,
+        run_on_start = true,
+        start_delay = 3000,
+      }
+    end,
   },
 }
