e8cf1a73c60ae4d91197a9b73c7f0019d2294510
SOV710 <chris916911179@outlook.com>
2026-04-02 09:10:55 +0000

    style(langs/format.lua): change double quotes to single quotes for consistency with Lua conventions feat(langs/format.lua): add notify_on_error option to display notifications when formatting fails refactor(langs/format.lua): reformat keys table for better readability and maintainability The changes standardize string quoting to single quotes, which is a common Lua style. Adding notify_on_error improves user experience by providing immediate feedback on formatting errors. The refactoring of the keys table enhances code clarity and aligns with common formatting practices in the codebase.

--- lua/plugins/langs/format.lua
+++ lua/plugins/langs/format.lua
@@ -1,22 +1,31 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
   opts = function()
     return {
-      formatters_by_ft = require("core.language").formatters,
+      notify_on_error = true,
+      formatters_by_ft = require('core.language').formatters,
       format_on_save = {
         timeout_ms = 3000,
-        lsp_format = "fallback",
+        lsp_format = 'fallback',
       },
     }
   end,
   keys = {
     {
-      "<leader>cf",
-      function() require("conform").format({ async = true }) end,
-      mode = { "n", "v" },
-      desc = "Format buffer",
+      '<leader>cf',
+      function()
+        require('conform').format { async = true }
+      end,
+      mode = { 'n', 'v' },
+      desc = 'Format buffer',
     },
   },
+  -- formatters = {
+  --   clangformat = {
+  --     command = 'clang-format',
+  --     args = '-style=file:/home/chris/.config/clang-format/.clang-format',
+  --   },
+  -- },
 }
