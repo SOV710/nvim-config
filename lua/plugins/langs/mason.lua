603340d4073bbb0e561a8eceedefe8f9720cdabd
SOV710 <chris916911179@outlook.com>
2026-04-02 09:54:12 +0000

    feat(mason): configure uv as python venv and installer backend

--- lua/plugins/langs/mason.lua
+++ lua/plugins/langs/mason.lua
@@ -1,25 +1,30 @@
 return {
   {
-    "mason-org/mason.nvim",
-    build = ":MasonUpdate",
-    opts = {},
+    'mason-org/mason.nvim',
+    build = ':MasonUpdate',
+    opts = {
+      python = {
+        venv = 'uv', -- "uv", "venv" or "virtualenv",
+        installer = 'uv', -- "uv" (default if venv == "uv") or "pip" (default if venv == "venv" or "virtualenv")
+      },
+    },
   },
   {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    dependencies = { "mason-org/mason.nvim" },
-    cmd = { "MasonToolsInstall", "MasonToolsUpdate" },
-    event = "VeryLazy",
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    dependencies = { 'mason-org/mason.nvim' },
+    cmd = { 'MasonToolsInstall', 'MasonToolsUpdate' },
+    event = 'VeryLazy',
     opts = function()
       return {
-        ensure_installed = require("core.language").mason,
+        ensure_installed = require('core.language').mason,
         auto_update = false,
         run_on_start = true,
         start_delay = 3000,
       }
     end,
     config = function(_, opts)
-      require("mason-tool-installer").setup(opts)
-      require("mason-tool-installer").run_on_start()
+      require('mason-tool-installer').setup(opts)
+      require('mason-tool-installer').run_on_start()
     end,
   },
 }
