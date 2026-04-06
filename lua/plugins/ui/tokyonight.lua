3eb9169f8e8adab3445e742e10379abbe6f7afef
SOV710 <chris916911179@outlook.com>
2026-04-06 23:36:57 +0000

    The comments were removed to reduce clutter and improve readability, as they were redundant and not providing essential information.

--- lua/plugins/ui/tokyonight.lua
+++ lua/plugins/ui/tokyonight.lua
@@ -1,8 +1,4 @@
-return { -- You can easily change to a different colorscheme.
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command in the config to whatever the name of that colorscheme is.
-  --
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+return {
   'folke/tokyonight.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   opts = {
@@ -14,9 +10,6 @@ return { -- You can easily change to a different colorscheme.
   },
   config = function(_, opts)
     require('tokyonight').setup(opts)
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
     vim.cmd.colorscheme 'tokyonight-night'
 
     -- You can configure highlights by doing something like:
