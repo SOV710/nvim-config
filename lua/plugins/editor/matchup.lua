88d25f4ec781630538faf085e31c12ad66a54da5
SOV710 <chris916911179@outlook.com>
2026-04-07 18:20:44 +0000

    The type annotation `---@type matchup.Config` is removed because it is outdated and no longer needed.

--- lua/plugins/editor/matchup.lua
+++ lua/plugins/editor/matchup.lua
@@ -17,7 +17,6 @@ return {
   end,
   -- or use the `opts` mechanism built into `lazy.nvim`. It calls
   -- `require('match-up').setup` under the hood
-  ---@type matchup.Config
   opts = {
     treesitter = {
       stopline = 1000,
