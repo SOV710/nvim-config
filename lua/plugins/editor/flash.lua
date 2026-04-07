20a0581a7e9f64726b8646828718a49061727b4a
SOV710 <chris916911179@outlook.com>
2026-04-07 17:26:51 +0000

    feat(flash.lua): add label exclude list, custom keys, and char_actions configuration Adds configuration options for the flash plugin to customize label exclusion, key mappings, and character action behavior. This provides more control over the flash motion interface, allowing users to tailor the plugin to their preferences and avoid conflicts with other keybindings.

--- lua/plugins/editor/flash.lua
+++ lua/plugins/editor/flash.lua
@@ -50,6 +50,17 @@ return {
         autohide = false, -- auto-hide labels after jump in char mode
         jump_labels = true, -- show jump labels for char motions
         multi_line = true, -- allow char motions across lines
+        label = { exclude = 'hjkliardc' }, -- When using jump labels, don't use these keys
+        keys = { 'f', 'F', 't', 'T', ';', ',' },
+        char_actions = function(motion)
+          return {
+            [';'] = 'next', -- set to `right` to always go right
+            [','] = 'prev', -- set to `left` to always go left
+            -- clever-f style
+            [motion:lower()] = 'next',
+            [motion:upper()] = 'prev',
+          }
+        end,
       },
       treesitter = {
         labels = 'asdfghjklqwertyuiopzxcvbnm',
