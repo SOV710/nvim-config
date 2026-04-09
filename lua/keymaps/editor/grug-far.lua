1c559cafa8eaf212dbc9d7693ff967309b0e970f
SOV710 <chris916911179@outlook.com>
2026-04-09 03:11:54 +0000

    fix(grug-far.lua): change keymap from <leader>S to <leader>r to resolve conflict with Resume picker The keymap is changed to avoid a conflict with the Resume picker, which also uses <leader>S. This ensures that both functionalities can be used without interference.

--- lua/keymaps/editor/grug-far.lua
+++ lua/keymaps/editor/grug-far.lua
@@ -1,14 +1,14 @@
 return {
   -- NOTE: changed from `<leader>sr` to `<leader>S` to avoid conflict with Resume picker
   {
-    '<leader>S',
+    '<leader>r',
     function()
       require('grug-far').open()
     end,
     desc = 'Search and replace',
   },
   {
-    '<leader>S',
+    '<leader>r',
     function()
       require('grug-far').with_visual_selection()
     end,
