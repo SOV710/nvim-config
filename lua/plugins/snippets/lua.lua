cecce5c4398bdef3f69f3083284e67e90d9d6730
SOV710 <chris916911179@outlook.com>
2026-04-10 22:54:35 +0000

    chore(snippets): remove lua.lua file containing luasnip snippet definitions remove dead codes

--- lua/plugins/snippets/lua.lua
+++
@@ -1,8 +0,0 @@
-local luasnip = require 'luasnip'
-local snippets = luasnip.snippet
-local text = luasnip.text_node
-local insert = luasnip.insert_node
-
-luasnip.add_snippets('lua', {
-  snippets('keyset', { text 'vim.keymap.set(', insert(1, 'mode, key, dest'), text ')' }),
-})
