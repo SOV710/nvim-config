e981b01ffdc9c8fc48340c5a6114a61cdfc47b73
SOV710 <chris916911179@outlook.com>
2026-04-10 16:06:52 +0000

    build(completion.lua): add build command for blink.cmp plugin The build command is added to ensure the plugin is built correctly when installed.

--- lua/plugins/langs/completion.lua
+++ lua/plugins/langs/completion.lua
@@ -2,6 +2,7 @@ return {
   'saghen/blink.cmp',
   version = '1.*',
   event = { 'InsertEnter', 'CmdlineEnter' },
+  build = 'cargo build --release',
   dependencies = {
     'L3MON4D3/LuaSnip',
   },
